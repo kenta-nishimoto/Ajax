5dabff45b48f91d33c3addb403e21ef4c9edc2ce
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2020-08-14 13:33:56 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,5 +13,26 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index
   end 
-  
+
+  # 下記は既読の操作を行なっとときに動くアクション
+  def checked
+    post = Post.find(params[:id])
+    # ↪︎パラメーターから送られてくるメモのidを代入している
+
+    if post.checked then
+      # ↪︎post.checkedという既読したか否かを判定するプロパティを指定している
+      post.update(checked: false)
+      # ↪︎既読していれば「既読を解除するためにfalseへ変更」
+    else
+      post.update(checked: true)
+      # ↪︎既読していなければ「既読にするためtrueへ変更」
+    end
+    # ↪︎この時はupdateというActiveRecordのメソッドを使用して更新しています。
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+    #↪︎更新したレコードをitem = Post.find(params[:id])で取得し直し、
+    # render json:{ post: item }でJSON形式（データ）としてchecked.jsに返却しています。
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 # get 'posts/new', to: 'posts#new'←これは削除する
   post 'posts', to: 'posts#create'
+  # 下記はメモのidを取得するために記述（pathパラメーターで記述）
+  get 'posts/:id', to: 'posts#checked'
 end
